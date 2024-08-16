0e6d974e8f47637fce706175b21d84c380a11401
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2024-08-16 13:40:58 +0600

    Sign in page done

--- lib/routes.dart
+++ lib/routes.dart
@@ -10,16 +10,14 @@ import 'screens/init_screen.dart';
 import 'screens/login_success/login_success_screen.dart';
 import 'screens/otp/otp_screen.dart';
 import 'screens/profile/profile_screen.dart';
-import 'screens/sign_in/sign_in_screen.dart';
 import 'screens/sign_up/sign_up_screen.dart';
-import 'screens/splash/splash_screen.dart';
 
 // We use name route
 // All our routes will be available here
 final Map<String, WidgetBuilder> routes = {
   InitScreen.routeName: (context) => const InitScreen(),
   // SplashScreen.routeName: (context) => const SplashScreen(),
-  SignInScreen.routeName: (context) => const SignInScreen(),
+  // SignInScreen.routeName: (context) => const SignInScreen(),
   ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
   LoginSuccessScreen.routeName: (context) => const LoginSuccessScreen(),
   SignUpScreen.routeName: (context) => const SignUpScreen(),
