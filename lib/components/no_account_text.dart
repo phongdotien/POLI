f00b78686eb507e0d30c2a109fb7f51f8c1baab2
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2024-08-16 20:39:52 +0600

    Forgot Password done

--- lib/components/no_account_text.dart
+++ lib/components/no_account_text.dart
@@ -18,7 +18,8 @@ class NoAccountText extends StatelessWidget {
           style: TextStyle(fontSize: 16),
         ),
         GestureDetector(
-          onTap: () => Navigator.pushNamed(context, SignUpScreen.routeName),
+          // onTap: () => Navigator.pushNamed(context, SignUpScreen.routeName),
+
           child: const Text(
             "Sign Up",
             style: TextStyle(fontSize: 16, color: Color(0xFFFF7643)),
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/home/home_screen.dart';
-import 'package:shop_app/screens/sign_up/complate_profile_screen.dart';
-import 'package:shop_app/screens/sign_up/sign_up_screen.dart';
+import 'package:shop_app/screens/sign_in/forgot_password_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +17,7 @@ class MyApp extends StatelessWidget {
       // theme: AppTheme.lightTheme(context),
       // initialRoute: SplashScreen.routeName,
       // routes: routes,
-      home: ComplateProfileScreen(),
+      home: ForgotPasswordScreen(),
     );
   }
 }
