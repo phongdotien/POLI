c9a90aa209d5fe64485c9e16a06e5a4f1c600ca4
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2024-08-16 20:29:38 +0600

    ComplateProfileScreen done

--- lib/screens/sign_up/complate_profile_screen.dart
+++ lib/screens/sign_up/complate_profile_screen.dart
@@ -174,64 +174,6 @@ class ComplateProfileForm extends StatelessWidget {
   }
 }
 
-class SocalCard extends StatelessWidget {
-  const SocalCard({
-    Key? key,
-    required this.icon,
-    required this.press,
-  }) : super(key: key);
-
-  final Widget icon;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        height: 56,
-        width: 56,
-        decoration: const BoxDecoration(
-          color: Color(0xFFF5F6F9),
-          shape: BoxShape.circle,
-        ),
-        child: icon,
-      ),
-    );
-  }
-}
-
-class NoAccountText extends StatelessWidget {
-  const NoAccountText({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text(
-          "Don’t have an account? ",
-          style: TextStyle(color: Color(0xFF757575)),
-        ),
-        GestureDetector(
-          onTap: () {
-            // Handle navigation to Sign Up
-          },
-          child: const Text(
-            "Sign Up",
-            style: TextStyle(
-              color: Color(0xFFFF7643),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
 // Icons
 const userIcon =
     '''<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
